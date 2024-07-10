ea436924abd5eb6c945dcca2965aa42d4beadfae
mh-skjelvareid <martin.hansen.skjelvareid@gmail.com>
2024-07-10 14:27:09 +0200

    Moving example dataset fixture to conftest

--- tests/test_processors.py
+++ tests/test_processors.py
@@ -1,28 +1,10 @@
-import shutil
 import zipfile
 from pathlib import Path
 
 import numpy as np
 import pytest
 
-import massipipe
 import massipipe.processors
-
-EXAMPLE_DATA_PATH = (
-    Path(__file__).parent
-    / "example_data"
-    / "massimal_larvik_olbergholmen_202308301001-test_hsi.zip"
-)
-
-
-@pytest.fixture(scope="session")
-def example_dataset_dir(tmp_path_factory):
-    """Create temporary folder with example data"""
-    tmp_dir = tmp_path_factory.mktemp("example_data")
-    with zipfile.ZipFile(EXAMPLE_DATA_PATH, mode="r") as zip_file:
-        zip_file.extractall(path=tmp_dir)
-    dataset_dir = tmp_dir.glob("*").__next__()  # Dataset "root" folder
-    return dataset_dir
 
 
 def test_radiance_calibration_dataset(example_dataset_dir):
