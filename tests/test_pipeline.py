c91e15b4c7d1660aa7feb4e15526a49476cc56cc
mh-skjelvareid <martin.hansen.skjelvareid@gmail.com>
2024-07-19 17:27:40 +0200

    Check correct number of files

--- tests/test_pipeline.py
+++ tests/test_pipeline.py
@@ -7,6 +7,9 @@ import massipipe.pipeline
 def test_pipeline_defaults(example_dataset_dir):
     pp = massipipe.pipeline.PipelineProcessor(example_dataset_dir)
     pp.run()
+    assert len(pp.raw_image_paths) == 2
+    assert len(pp.base_file_names) == 2
+    assert len(pp.refl_im_paths) == 2
     assert all(f.exists() for f in pp.rad_im_paths)
     assert all(f.exists() for f in pp.irrad_spec_paths)
     assert all(f.exists() for f in pp.refl_im_paths)
