8057c699a5b4b039e0448634fee0580a4d1fb630
mh-skjelvareid <martin.hansen.skjelvareid@gmail.com>
2024-10-12 23:57:45 +0200

    Finishing Pydantic config setup (first version)

--- massipipe/config.py
+++ massipipe/config.py
@@ -2,7 +2,7 @@ from datetime import datetime
 from typing import List, Literal, Optional, Tuple, Union
 
 import yaml
-from pydantic import BaseModel, PositiveFloat, PositiveInt
+from pydantic import BaseModel, PositiveFloat, PositiveInt, field_validator
 
 
 def parse_config(yaml_path):
@@ -62,9 +62,67 @@ class MpRadianceGc(BaseModel):
     ] = None
 
 
+class MpRadianceGcRgb(BaseModel):
+    create: bool = True
+    overwrite: bool = False
+
+
+class MpIrradiance(BaseModel):
+    create: bool = True
+    overwrite: bool = False
+
+
+class MpReflectance(BaseModel):
+    create: bool = True
+    overwrite: bool = False
+    wl_min: Optional[float] = None
+    wl_max: Optional[float] = None
+    conv_irrad_with_gauss: bool = True
+    fwhm_irrad_smooth: Optional[float] = None
+    smooth_spectra: bool = False
+    add_map_info: bool = True
+    refl_from_mean_irrad: bool = False
+
+
+class MpReflectanceGc(BaseModel):
+    create: bool = True
+    overwrite: bool = False
+    smooth_spectra: bool = False
+    wl_min: Optional[float] = None
+    wl_max: Optional[float] = None
+
+
+class MpReflectanceGcRgb(BaseModel):
+    create: bool = True
+    overwrite: bool = False
+
+
+class MpMosaicCreateOverwrite(BaseModel):
+    create: bool = True
+    overwrite: bool = True
+
+
+class MpMosaic(BaseModel):
+    overview_factors: Optional[Union[list[int], tuple[int]]] = None
+    radiance_rgb: MpMosaicCreateOverwrite
+    radiance_gc_rgb: MpMosaicCreateOverwrite
+    reflectance_gc_rgb: MpMosaicCreateOverwrite
+
+
 class MassipipeOptions(BaseModel):
     general: MpGeneral
     quicklook: MpQuickLook
+    imu_data: MpImuData
+    geotransform: MpGeoTransform
+    radiance: MpRadiance
+    radiance_rgb: MpRadianceRgb
+    radiance_gc: MpRadianceGc
+    radiance_gc_rgb: MpRadianceGcRgb
+    irradiance: MpIrradiance
+    reflectance: MpReflectance
+    reflectance_gc: MpReflectanceGc
+    reflectance_gc_rgb: MpReflectanceGcRgb
+    mosaic: MpMosaic
 
 
 class Config(BaseModel):
@@ -79,3 +137,12 @@ class Config(BaseModel):
     spectrum_type: Literal["RGB", "MSI", "HSI"] = "HSI"
     creator_name: str
     massipipe_options: MassipipeOptions
+
+    @field_validator("datetime", mode="before")
+    def datetime_validate(cls, datetime_str: str):
+        if len(datetime_str) == 8:  # Expect YYYYmmdd
+            return datetime.strptime(datetime_str, "%Y%m%d")
+        elif len(datetime_str) == 12:  # Expect YYYYmmddHHMM
+            return datetime.strptime(datetime_str, "%Y%m%d%H%M")
+        else:
+            raise ValueError(f"Invalid datetime string, use YYYYmmdd or YYYYmmddHHMM")
