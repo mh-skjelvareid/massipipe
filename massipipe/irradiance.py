Fixing / clarifying conversions between irradiance units

massipipe/irradiance.py
@@ -143,13 +143,11 @@ class IrradianceConverter:
 
         # Subtract dark current, multiply with radiance conversion spectrum
         # NOTE: Resonon irradiance unit is uW/(pi*cm2*um) = 10e-5 W/(pi*m2*nm)
-        # TODO: Check if pi in unit above is correct (probably not)
+        # Scaling with pi is at least consistent with Resonon code.
         cal_irrad_spec = (raw_spec - self._cal_dark_spec) * scaled_conv_spec
 
         # Convert to standard spectral irradiance unit W/(m2*nm)
-        # TODO: Conversion below does not make sense
-        # W/(m2*nm) = 10^5 * uW/(cm2*um)
-        cal_irrad_spec = cal_irrad_spec * (np.pi / 100_000)
+        cal_irrad_spec = mpu.irrad_uflicklike_to_si_nm(cal_irrad_spec * np.pi)
 
         # Set spectrum outside wavelength limits to zero
         if set_irradiance_outside_wl_limits_to_zero:

massipipe/reflectance.py
@@ -270,6 +270,7 @@ class ReflectanceConverter:
 
         _, rad_wl = mpu.read_envi_header(radiance_image_header)
         irrad_spec_interp = self._interpolate_irrad_to_image_wl(irrad_spec, irrad_wl, rad_wl)
+        irrad_spec_interp = mpu.irrad_si_nm_to_si_um(irrad_spec_interp)  # Convert to W/(m2*um)
         mpu.add_header_irradiance(irrad_spec_interp, radiance_image_header)
 
     def convert_radiance_file_with_irradiance_to_reflectance(
@@ -308,9 +309,7 @@ class ReflectanceConverter:
         irrad_spec = irrad_spec[valid_image_wl_ind]
 
         # Make irradiance spectrum compatible with image
-        # TODO: Verify that conversion below is correct!
-        irrad_spec = irrad_spec * 1000  # Convert from W/(m2*um) to W/(m2*nm)
-        irrad_spec = irrad_spec * 100_000  # Convert from W/(m2*nm) to uW/(cm2*um)
+        irrad_spec = mpu.irrad_si_um_to_uflicklike(irrad_spec)  # Convert W/(m2*um) to uW/(cm2*um)
         irrad_spec = np.expand_dims(irrad_spec, axis=(0, 1))
 
         # Convert to reflectance, assuming Lambertian (perfectly diffuse) surface