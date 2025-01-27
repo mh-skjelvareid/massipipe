f19b1c82d9a8b8c31256c914f0d08464d8f01e05
mh-skjelvareid <martin.hansen.skjelvareid@gmail.com>
2025-01-27 21:29:08 +0100

    Changing default visualization mosaic to radiance

--- massipipe/config.py
+++ massipipe/config.py
@@ -114,7 +114,7 @@ class MpMosaicCreateOverwrite(BaseModel):
 
 class MpMosaic(BaseModel):
     overview_factors: Sequence[PositiveInt] = [2, 4, 8, 16, 32]
-    visualization_mosaic: Literal["radiance", "radiance_gc"] = "radiance_gc"
+    visualization_mosaic: Literal["radiance", "radiance_gc"] = "radiance"
     radiance_rgb: MpMosaicCreateOverwrite
     radiance_gc_rgb: MpMosaicCreateOverwrite
     # reflectance_rgb: MpMosaicCreateOverwrite # Not yet implemented
