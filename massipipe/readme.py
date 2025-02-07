09c41f7f347c965cf775b54b079fa14b1b9bbe45
mh-skjelvareid <martin.hansen.skjelvareid@gmail.com>
2025-02-07 13:15:31 +0100

    Added content to readme text

--- massipipe/readme.py
+++ massipipe/readme.py
@@ -42,6 +42,7 @@ Additional information about the project can be found on the following websites:
 - [Cristin research database project page](https://app.cristin.no/projects/show.jsf?id=2054355)
 - [Norwegian Research Council project
   page](https://prosjektbanken.forskningsradet.no/project/FORISS/301317)
+- [SeaBee data portal with Massimal data](https://geonode.seabee.sigma2.no/catalogue/#/search?q=massimal&f=dataset)
 
 ## Hyperspectral imaging system
 Hyperspectral imaging was performed using a "[Airborne Remote Sensing System](https://resonon.com/hyperspectral-airborne-remote-sensing-system)"
@@ -58,13 +59,62 @@ the spectral range of 390-1030 nm spans the 600 pixels along the short axis of t
 sensor. By default, pairs of spectral pixels are binned, resulting in 300 spectral
 channels. In the specifications of the camera, Resonon lists that it has a spectral
 range of 400-1000 nm and 281 spectral channels, indicating that the channels at the high
-and low ends of the spectra are usually discarded. The spectral resolution (FWHM) is
-approximately 2.7 nm. The 900 pixels along the long axis of the sensor corresponds to
-900 spatial pixels. Each image frame corresponds to imaging of a single line on the
-ground, and to create a 2D image where each pixel has 300 spectral channels, the camera
-has to be moved across the area of interest. The along-track sampling distance on the
-ground is given by the speed of the camera multiplied by the image acquisition time for
-each frame.
+and low ends of the spectra are usually discarded. The 900 pixels along the long axis of
+the sensor corresponds to 900 spatial pixels. Each image frame corresponds to imaging of
+a single line on the ground, and to create a 2D image where each pixel has 300 spectral
+channels, the camera has to be moved across the area of interest. The along-track
+sampling distance on the ground is given by the speed of the camera multiplied by the
+image acquisition time for each frame.
+
+The spectral resolution (FWHM) has a mean value of approximately 2.7 nm. The table
+below lists the FWHM for a selection of channels. The data is from Resonons general
+design documents. Each individual camera may have slight deviations from this, but
+Resonon claims that the data fits experiments well. Note the local minimum of 2.24 nm at
+approx. 470 nm and the local maximum of 2.94 nm at approx. 745 nm.
+
+| Wavelength (nm) | FWHM (nm) |
+| :-------------: | :-------: |
+|             400 |      2.97 |
+|             415 |      2.55 |
+|             430 |      2.36 |
+|             445 |      2.27 |
+|             460 |      2.24 |
+|             475 |      2.24 |
+|             490 |      2.26 |
+|             505 |      2.29 |
+|             520 |      2.33 |
+|             535 |      2.39 |
+|             550 |      2.44 |
+|             565 |      2.50 |
+|             580 |      2.57 |
+|             595 |      2.63 |
+|             610 |      2.68 |
+|             625 |      2.74 |
+|             640 |      2.79 |
+|             655 |      2.83 |
+|             670 |      2.86 |
+|             685 |      2.89 |
+|             700 |      2.92 |
+|             715 |      2.93 |
+|             730 |      2.94 |
+|             745 |      2.94 |
+|             760 |      2.94 |
+|             775 |      2.93 |
+|             790 |      2.91 |
+|             805 |      2.89 |
+|             820 |      2.87 |
+|             835 |      2.84 |
+|             850 |      2.80 |
+|             865 |      2.77 |
+|             880 |      2.73 |
+|             895 |      2.69 |
+|             910 |      2.65 |
+|             925 |      2.61 |
+|             940 |      2.57 |
+|             955 |      2.53 |
+|             970 |      2.50 |
+|             985 |      2.47 |
+|            1000 |      2.49 |
 
 ### On-board computer
 A small on-board computer made by Resonon was used for controlling the camera, logging sensor data, and communicating with the ground station. The computer ran a Linux-based imaging firmware. 
@@ -151,6 +201,85 @@ For example, if the UAV was above (dark) water at the time of autoexposure and t
 over (bright) land before the autoexposure could be recalculated, parts of the image
 became saturated, resulting in invalid pixels. 
 
+## Massipipe data processing pipeline
+The dataset has been processed by
+[MassiPipe](https://github.com/mh-skjelvareid/massipipe), a data processing pipeline
+developed as part of the MASSIMAL project. Developent of the pipeline has been based on
+data from the Resonon Pika-L hyperspectral camera, but many elements of the pipeline (e.g.
+reflectance calculation, sun glint correction) are general and can be applied to any
+hyperspectral image.  
+
+MassiPipe can be used to automatically generate additional image products, based on the
+radiance images distributed in the dataset. See details in the description of dataset
+contents below. 
+
+## Dataset contents
+The following is a general description which is valid for most of the datasets produced
+by the MASSIMAL project. Some deviations from this description may exist for special
+cases, usually caused by technical problems during field work.
+
+The same base file name is used for multiple types of data (e.g. IMU data and images),
+this enables identification of which files belong together. 
+
+
+### Configuration file - config.seabee.yaml 
+The configuration file contains metadata about the dataset, and the parameters for
+processing the dataset using MassiPipe. The file name includes "seabee" because the
+dataset was perpared for publication and interactive exploration through the SeaBee data
+portal, run by [SeaBee](https://seabee.no/) (Norwegian Infrastructure for Drone-based
+Research, Mapping and Monitoring in the Coastal Zone).
+
+For explanation of the MassiPipe processing parameters, see the [MassiPipe
+documentation](https://mh-skjelvareid.github.io/massipipe/). The configuration file can
+be edited to re-run the processing with modified parameters, and/or produce additional
+image products. 
+
+### Quicklook
+The "quicklook" images are color images saved in the PNG format, to be used for getting
+a quick overview of the dataset. The data displayed as red, green and blue (RGB)
+corresponds to slices of the hyperspectral radiance image extracted at the "RGB
+wavelengths" set in the config file. Each color channel has also been individually
+contrast stretched, using the 2nd and 98th percentiles of the original data to set the
+lower and upper ends of the range of values displayed. Note that since the image
+statistics change from image to image, identical objects or nature types may appear
+different in different images. The images are also not georeferenced. It is not
+recommended to use the quicklook images for any type of analysis - use the hyperspectral
+data instead. 
+
+### IMU data
+IMU data is stored as JSON files with 7 fields:
+- **time**: Time represented as a single floating-point value, describing the number of
+  seconds passed since January 1st 1970 (["UNIX
+  time"](https://en.wikipedia.org/wiki/Unix_time)) 
+- **roll**: Camera roll measured in radians. Positive values correspond to
+  "right wing up", or pointing the camera to the right side of the flight line. 
+- **pitch**: Camera pitch measured in radians. 
+- **yaw**: Camera heading, measured in radians. Zero at due north, $\pi/2$ at due east.
+- **longitude**: Longitude in decimal degrees, positive for east longitude
+- **latitude**: Latitude in decimal degrees, positive for northern hemisphere
+- **altitude**: Altitude in meters relative to the WGS-84 ellipsiod.
+
+All values have been interpolated to match the lines of the hyperspectral image. E.g., if the
+image contains 2000 lines, each field has 2000 corresponding values. 
+
+*Note that while roll, pitch, longitude and latitude are relatively accurate, yaw and
+altitude are less so.* The magnetometer which the IMU used to measure yaw (heading) turned out to
+have poor accuracy, despite repeated calibrations. Absolute altitude values also appear
+to be accurate only to approximately +/- 10 m, probably due to limited accuracy when
+estimating altitude from GPS / GNSS. However, relative altitude values for the same
+flight (same dataset) are fairly consistent, probably because altitude estimation was
+aided by a barometric pressure sensor.  
+
+
+### Radiance hyperspectral images
+
+### Downwelling irradiance
+
+### Mosaic
+
+
+ 
+
 """
 
 
@@ -158,6 +287,6 @@ def write_readme(readme_file_path: Path):
     """Write standard readme text to file"""
     try:
         with open(readme_file_path, mode="w", encoding="utf-8") as readme_file:
-            readme_file.write(README_TEXT)
+            readme_file.write(TEXT)
     except Exception:
         logger.error(f"Error while writing readme text to {readme_file_path}", exc_info=True)
