407f9d46acc2712a207f886b0214bbc9b48c57f4
mh-skjelvareid <martin.hansen.skjelvareid@gmail.com>
2025-12-06 12:25:26 +0100

    Rewriting based on new notation

--- massipipe/orthorectification.py
+++ massipipe/orthorectification.py
@@ -12,9 +12,8 @@ class CameraModel:
         self,
         opening_angle_deg: float,
         n_pix: int,
-        cam_to_imu_rot_dcm: NDArray | None = None,
-        cam_to_imu_rot_euler: NDArray | None = None,
-        estimate_yaw_from_positions: bool = False,
+        R_to_imu_from_cam: NDArray | None = None,
+        euler_to_imu_from_cam: NDArray | None = None,
     ):
         """
         Initialize the orthorectification parameters.
@@ -25,14 +24,12 @@ class CameraModel:
             The opening angle of the camera in degrees.
         n_pix : int
             The number of pixels per image line.
-        cam_to_imu_rot_dcm : NDArray, optional
+        R_to_imu_from_cam : NDArray, optional
             Direction Cosine Matrix (DCM) representing the rotation from the camera
-            to the IMU. If provided, `cam_to_imu_rot_euler` must not be provided.
-        cam_to_imu_rot_euler : NDArray, optional
+            to the IMU. If provided, `euler_to_imu_from_cam` must not be provided.
+        euler_to_imu_from_cam : NDArray, optional
             Euler angles (yaw, pitch, roll) representing the rotation from the camera
             to the IMU. If provided, `cam_to_imu_rot_dcm` must not be provided.
-        estimate_yaw_from_positions : bool, optional
-            Whether to estimate the yaw angle from positions. Default is False.
 
         Raises
         ------
@@ -43,44 +40,40 @@ class CameraModel:
         self.opening_angle_deg = opening_angle_deg
         self.n_pix = n_pix
 
-        if cam_to_imu_rot_dcm is not None:
-            if cam_to_imu_rot_euler is not None:
+        if R_to_imu_from_cam is not None:
+            if euler_to_imu_from_cam is not None:
                 raise ValueError("Provide rotation as angles or DCM, not both.")
             else:
-                self.cam_to_imu_rot = Rotation.from_matrix(cam_to_imu_rot_dcm)
-        elif cam_to_imu_rot_euler is not None:
-            yaw, pitch, roll = cam_to_imu_rot_euler
-            self.cam_to_imu_rot = Rotation.from_euler("zyx", [yaw, pitch, roll])
+                self.R_to_imu_from_cam = Rotation.from_matrix(R_to_imu_from_cam)
+        elif euler_to_imu_from_cam is not None:
+            yaw, pitch, roll = euler_to_imu_from_cam
+            self.R_to_imu_from_cam = Rotation.from_euler("zyx", [yaw, pitch, roll])
         else:
-            self.cam_to_imu_rot = Rotation.from_euler("zyx", [0.0, 0.0, 0.0])
+            self.R_to_imu_from_cam = Rotation.from_euler("zyx", [0.0, 0.0, 0.0])  # No rotation
 
-        self.estimate_yaw_from_positions = estimate_yaw_from_positions
-        if self.estimate_yaw_from_positions:
-            raise NotImplementedError("Yaw estimation from positions is not yet implemented.")
+    # def camera_rotations(self, yaw: NDArray, pitch: NDArray, roll: NDArray):
+    #     """
+    #     Computes the camera rotations by applying a transformation from IMU rotations.
 
-    def camera_rotations(self, yaw: NDArray, pitch: NDArray, roll: NDArray):
-        """
-        Computes the camera rotations by applying a transformation from IMU rotations.
+    #     Parameters
+    #     ----------
+    #     yaw : NDArray
+    #         Array of yaw angles (rotation around the Z-axis) in radians.
+    #     pitch : NDArray
+    #         Array of pitch angles (rotation around the Y-axis) in radians.
+    #     roll : NDArray
+    #         Array of roll angles (rotation around the X-axis) in radians.
 
-        Parameters
-        ----------
-        yaw : NDArray
-            Array of yaw angles (rotation around the Z-axis) in radians.
-        pitch : NDArray
-            Array of pitch angles (rotation around the Y-axis) in radians.
-        roll : NDArray
-            Array of roll angles (rotation around the X-axis) in radians.
+    #     Returns
+    #     -------
+    #     Rotation
+    #         A batch of M rotation matrices representing the camera rotations.
+    #         R_cam_to_world = R_imu_to_world @ R_cam_to_imu
+    #     """
+    #     imu_rot = Rotation.from_euler("zyx", np.column_stack([yaw, pitch, roll]))
+    #     return imu_rot * self.cam_to_imu_rot  # Matrix multiplications (batch of M)
 
-        Returns
-        -------
-        Rotation
-            A batch of M rotation matrices representing the camera rotations.
-            R_cam_to_world = R_imu_to_world @ R_cam_to_imu
-        """
-        imu_rot = Rotation.from_euler("zyx", np.column_stack([yaw, pitch, roll]))
-        return imu_rot * self.cam_to_imu_rot  # Matrix multiplications (batch of M)
-
-    def pixel_looking_angles(self) -> NDArray:
+    def calc_pixel_looking_angles(self) -> NDArray:
         """Calculate the looking angles for each pixel in the sensor.
 
         Returns
@@ -92,7 +85,7 @@ class CameraModel:
         edge = np.tan(np.radians(self.opening_angle_deg / 2))
         return np.arctan(np.linspace(-edge, edge, self.n_pix))
 
-    def ray_rotations(self, cam_rot: Rotation) -> NDArray:
+    def calc_ray_rotation_matrices(self, yaw: NDArray, pitch: NDArray, roll: NDArray) -> NDArray:
         """
         Compute the ray rotation matrices for a camera given its rotation and pixel looking angles.
 
@@ -109,111 +102,105 @@ class CameraModel:
             A NumPy array of shape (M, N, 3, 3) representing the ray rotation matrices.
             M is the number of camera rotations, and N is the number of pixel looking angles.
         """
-        # TODO: Double-check the order of rotation matrices here
-        cam_rot_mat = cam_rot.as_matrix()  # (M, 3, 3)
-        pixel_rot_mat = Rotation.from_euler("x", self.pixel_looking_angles()).as_matrix()
 
-        return cam_rot_mat[:, np.newaxis, :, :] @ pixel_rot_mat[np.newaxis, ::, :, :]  # (M,N,3,3)
+        # Create rotation objects
+        R_to_world_from_imu = Rotation.from_euler("zyx", np.column_stack([yaw, pitch, roll]))
+        R_to_world_from_cam = R_to_world_from_imu * self.R_to_imu_from_cam
+        R_to_cam_from_pixel = Rotation.from_euler("x", self.calc_pixel_looking_angles())
 
-    def intersect_rays_ground(self, ray_rot: NDArray, cam_alt: NDArray) -> NDArray:
+        # Convert to matrices
+        R_world_cam = R_to_world_from_cam.as_matrix()  # (M,3,3)
+        R_cam_pixel = R_to_cam_from_pixel.as_matrix()  # (N,3,3)
+
+        # Matrix multiplication (last two dims) via broadcasting
+        return R_world_cam[:, np.newaxis, :, :] @ R_cam_pixel[np.newaxis, :, :, :]  # (M,N,3,3)
+
+    def intersect_rays_ground(
+        self,
+        R_to_world_from_pixel: NDArray,
+        cam_altitude: NDArray,
+    ) -> NDArray:
         """
         Intersects rays with the ground plane to calculate the northing and easting offsets.
 
         Parameters
         ----------
-        ray_rot : NDArray
-            A 3D array of shape (N, M, 3, 3) representing the rotation matrices for the rays.
+        R_to_world_from_pixel : NDArray
+            An array of shape (N, M, 3, 3) representing the rotation matrices for each pixel.
         cam_alt : NDArray
             A 1D array of shape (M,) representing the altitudes of the camera relative to the ground.
 
         Returns
         -------
         NDArray
-            A 3D array of shape (M, N, 2) containing the northing and easting offsets for each ray.
+            A 3D array of shape (M, N, 2) containing the direction vectors from the camera to the ground.
 
         """
-
-        # TODO: Double-check the multiplcation order for rotation matrices here
-        # The nadir ray should be rotated first in the camera frame, then to the IMU frame,
-        # then to the world frame
-
-        # Create unit vectors for each ray
-        ray_nadir = np.array([[0.0, 0.0, 1.0]]).T  # (3,1)
-        rays = ray_rot @ ray_nadir  # (N,M,3,1)
 
         # Parameterization: t corresponds to number of unit vectors to reach ground
         with np.errstate(divide="ignore", invalid="ignore"):
-            t = cam_alt[:, np.newaxis] / rays[:, :, 2, 1]  # (M,N)
+            t = cam_altitude[:, np.newaxis] / R_to_world_from_pixel[:, :, 2, 2]  # (M,N)
 
-        # Calculate northing and easting offsets
-        return rays[:, :, 0:2, 0] * t[:, :, np.newaxis]  # (M,N,2)
+        # Direction vector corresponds to last column of rotation matrix
+        # (multiplication with unit vector along z-axis, [0,0,1])
+        d_hat = R_to_world_from_pixel[:, :, 2, 0:2]  # (M,N,2)
+
+        # Extend direction vector down to ground plane
+        return d_hat * t[:, :, np.newaxis]
 
     def combine_camera_pos_ray_offsets(
-        self, camera_lat: NDArray, camera_long: NDArray, ray_offsets: NDArray
-    ) -> tuple[NDArray, int]:
+        self, camera_northing: NDArray, camera_easting: NDArray, ray_vectors: NDArray
+    ) -> NDArray:
         """
         Combine camera positions and ray offsets to compute pixel positions in UTM coordinates.
-
-        Parameters
-        ----------
-        camera_lat : NDArray
-            Array of camera latitude positions (in degrees).
-        camera_long : NDArray
-            Array of camera longitude positions (in degrees).
-        ray_offsets : NDArray
-            Array of ray offsets in UTM coordinates (shape: (M, N, 2)).
-
-        Returns
-        -------
-        tuple[NDArray, int]
-            A tuple containing:
-            - pixel_utm_ne (NDArray): Array of pixel positions in UTM coordinates (shape: (M, N, 2)).
-            - utm_epsg (int): EPSG code of the UTM zone used for the conversion.
         """
+        # Stack camera positions
+        r_camera = np.column_stack((camera_northing, camera_easting))[:, np.newaxis, :]  # (M,1,2)
 
-        cam_utm_e, cam_utm_n, utm_epsg = mpu.convert_long_lat_to_utm(camera_long, camera_lat)
-        cam_utm_ne = np.column_stack((cam_utm_n, cam_utm_e))[:, np.newaxis, :]  # (M,1,2)
-        pixel_utm_ne = cam_utm_ne + ray_offsets  # (M,N,2)
-        return pixel_utm_ne, utm_epsg
+        # Extract ray offsets in northing and easting
+        d_xy = ray_vectors[:, :, 0:2]  # (M,N,2)
+
+        # Combine camera positions and ray offsets via broadcasting
+        return r_camera + d_xy
 
     def pixel_ground_positions(
         self,
-        camera_lat: NDArray,
-        camera_long: NDArray,
+        camera_northing: NDArray,
+        camera_easting: NDArray,
+        camera_altitude: NDArray,
         camera_yaw: NDArray,
         camera_pitch: NDArray,
         camera_roll: NDArray,
-        camera_alt: NDArray,
-    ) -> tuple[NDArray, int]:
+    ) -> NDArray:
         # Check that all inputs have same length
-        M = len(camera_lat)
+        M = len(camera_northing)
         if not all(
-            len(x) == M for x in [camera_long, camera_yaw, camera_pitch, camera_roll, camera_alt]
+            len(x) == M
+            for x in [camera_easting, camera_yaw, camera_pitch, camera_roll, camera_altitude]
         ):
             raise ValueError("All input arrays must have the same length")
 
         # Get camera rotations
-        cam_rot = self.camera_rotations(camera_yaw, camera_pitch, camera_roll)
+        R_to_world_from_pixel = self.calc_ray_rotation_matrices(
+            camera_yaw, camera_pitch, camera_roll
+        )
 
-        # Calculate ray rotation matrices
-        ray_rot = self.ray_rotations(cam_rot)
-
-        # Calculate ray intersections (northing/easting offsets relative to camera)
-        ray_offsets = self.intersect_rays_ground(ray_rot, camera_alt)
+        # Calculate ray direction vectors
+        ray_vecs = self.intersect_rays_ground(R_to_world_from_pixel, camera_altitude)
 
         # Calculate pixel world coordinates
-        return self.combine_camera_pos_ray_offsets(camera_lat, camera_long, ray_offsets)
+        return self.combine_camera_pos_ray_offsets(camera_northing, camera_easting, ray_vecs)
 
-    def pixel_ground_positions_from_json(self, imu_json_path: Path | str):
-        imu_data = mpu.read_json(imu_json_path)
-        expected_keys = ["time", "latitude", "longitude", "altitude", "yaw", "pitch", "roll"]
-        if not all(x in imu_data for x in expected_keys):
-            raise ValueError(f"IMU JSON file must correspond to dict with keys {expected_keys}")
-        return self.pixel_ground_positions(
-            imu_data["latitude"],
-            imu_data["longitude"],
-            imu_data["yaw"],
-            imu_data["pitch"],
-            imu_data["roll"],
-            imu_data["altitude"],
-        )
+    # def pixel_ground_positions_from_json(self, imu_json_path: Path | str):
+    #     imu_data = mpu.read_json(imu_json_path)
+    #     expected_keys = ["time", "latitude", "longitude", "altitude", "yaw", "pitch", "roll"]
+    #     if not all(x in imu_data for x in expected_keys):
+    #         raise ValueError(f"IMU JSON file must correspond to dict with keys {expected_keys}")
+    #     return self.pixel_ground_positions(
+    #         imu_data["latitude"],
+    #         imu_data["longitude"],
+    #         imu_data["yaw"],
+    #         imu_data["pitch"],
+    #         imu_data["roll"],
+    #         imu_data["altitude"],
+    #     )
