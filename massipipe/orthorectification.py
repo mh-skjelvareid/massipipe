39fa58521e3503613eda6e278b195069d242fe4a
Martin H. Skjelvareid <martin.hansen.skjelvareid@gmail.com>
2025-12-13 00:15:33 +0100

    Final parts of new structure (first version)

--- massipipe/orthorectification.py
+++ massipipe/orthorectification.py
@@ -1,3 +1,5 @@
+from pathlib import Path
+
 import numpy as np
 import rasterio
 from numpy.typing import NDArray
@@ -6,7 +8,8 @@ from pyresample.geometry import AreaDefinition, SwathDefinition
 from pyresample.kd_tree import resample_nearest
 from rasterio.plot import reshape_as_raster
 from rasterio.profiles import DefaultGTiffProfile
-from rasterio.transform import from_bounds
+from rasterio.transform import Affine, from_bounds
+from scipy.interpolate import make_smoothing_spline
 from scipy.spatial.transform import Rotation
 
 import massipipe.utils as mpu
@@ -19,6 +22,7 @@ class CameraModel:
         n_pix: int,
         R_to_imu_from_cam: NDArray | None = None,
         euler_to_imu_from_cam: NDArray | None = None,
+        altitude_offset: float = 0,
     ):
         """
         Initialize the orthorectification parameters.
@@ -36,6 +40,11 @@ class CameraModel:
             Euler angles (yaw, pitch, roll), in radians, representing the rotation
             from the camera to the IMU. If provided, `cam_to_imu_rot_dcm` must not
             be provided.
+        altitude_offset : float, optional
+            Offset between the true altitude above ground and the altitude measured by
+            the IMU, in meters.
+                imu_altitude = true_altitude + altitude_offset
+                true_altitude = imu_altitude - altitude_offset
 
         Notes
         -----
@@ -61,6 +70,7 @@ class CameraModel:
             self.R_to_imu_from_cam = Rotation.from_euler("zyx", [yaw, pitch, roll])
         else:
             self.R_to_imu_from_cam = Rotation.from_euler("zyx", [0.0, 0.0, 0.0])  # No rotation
+        self.altitude_offset = altitude_offset
 
     @property
     def pixel_looking_angles(self) -> NDArray:
@@ -117,8 +127,10 @@ class CameraModel:
         R_to_world_from_pixel : NDArray
             An array of shape (N, M, 3, 3) representing the rotation matrices for each pixel.
         camera_altitude : NDArray
-            A 1D array of shape (M,) representing the altitude of the camera relative to the ground
-            for each image row.
+            A 1D array of shape (M,) representing the IMU measurements of altitude of the camera
+            relative to the ground for each image row. The CameraModel's altitude offset is used to
+            calculate true altitude as:
+                true_altitude = camera_altitude - camera_model.altitude_offset
 
         Returns
         -------
@@ -127,6 +139,9 @@ class CameraModel:
             ground for eah pixel.
 
         """
+
+        # Adjust camera altitude using altitude offset
+        camera_altitude = camera_altitude - self.altitude_offset
 
         # Parameterization: t corresponds to number of unit vectors to reach ground
         with np.errstate(divide="ignore", invalid="ignore"):
@@ -230,7 +245,7 @@ class Resampler:
             positions. The resolution is estimated median spacing of x and y coordinates
             in the pixel positions.
         """
-        gsd = estimate_resolution_from_pixel_coordinates(pixel_utm_positions)
+        gsd = resolution_from_pixel_coordinates(pixel_utm_positions)
         area_extent = area_extent_from_pixel_coordinates(pixel_utm_positions)
         x_min, y_min, x_max, y_max = area_extent
         width = int((x_max - x_min) / gsd)
@@ -312,11 +327,9 @@ class GeoTiffWriter:
     def __init__(self, nodata):
         self.nodata = nodata
 
-    def create_profile(
-        self, image: NDArray, area_extent: tuple[float, float, float, float], epsg: int
-    ) -> dict:
+    def _create_profile(self, image: NDArray, transform: Affine, epsg: int) -> dict:
         h, w, b = image.shape
-        tr = from_bounds(*area_extent, width=w, height=h)
+
         profile = dict(DefaultGTiffProfile())
         profile.update(
             {
@@ -325,21 +338,219 @@ class GeoTiffWriter:
                 "count": b,
                 "dtype": str(image.dtype),
                 "crs": CRS.from_epsg(epsg),
-                "transform": tr,
+                "transform": transform,
                 "nodata": self.nodata,
             }
         )
         return profile
 
-    def save(self, image: NDArray, wavelengths: NDArray, profile: dict, path: str):
+    def _save(self, image: NDArray, wavelengths: NDArray, profile: dict, path: str | Path):
         band_names = [f"{wl:.3f}" for wl in wavelengths]
         with rasterio.open(path, "w", **profile) as ds:
             for i, name in enumerate(band_names, 1):
                 ds.set_band_description(i, name)
             ds.write(reshape_as_raster(image))
 
+    def save_image(
+        self, image: NDArray, wavelengths: NDArray, transform: Affine, epsg: int, path: str | Path
+    ):
+        """Save orthorectified image as GeoTIFF file.
 
-def estimate_resolution_from_pixel_coordinates(
+        Parameters
+        ----------
+        image : NDArray
+            Orthorectified image to be saved, shape (n_northing, n_easting, n_bands).
+        wavelengths : NDArray
+            Wavelengths corresponding to each band in the image, shape (n_bands,).
+        profile : dict
+            Rasterio profile for the GeoTIFF file.
+        path : str
+            Path to the output GeoTIFF file.
+        """
+
+        # Create GeoTIFF profile
+        geotiff_profile = self._create_profile(image, transform, epsg)
+
+        # Save orthorectified image as GeoTIFF
+        self._save(image, wavelengths, geotiff_profile, path)
+
+
+class FlatTerrainOrthorectifier_2:
+    """Line-by-line orthorectification of hyperspectral images assuming flat terrain"""
+
+    def __init__(
+        self,
+        camera_fov: float,
+        camera_cross_track_n_pixels: int,
+        imu_camera_rotation_dcm: NDArray | None = None,
+        imu_camera_rotation_euler: NDArray | None = None,
+        camera_altitude_offset: float = 0,
+        radius_of_influence: float | None = None,
+        nodata_fill_value: float = np.nan,
+        ground_sampling_distance: float | None = None,
+        estimate_yaw_from_positions: bool = False,
+    ):
+        """Initialize orthorectifier with constants
+
+        Parameters
+        ----------
+        camera_fov : float,
+            Opening angle (radians) of pushbroom camera
+        camera_cross_track_n_pixels : int
+            Number of spatial pixels in pushbroom camera
+        imu_camera_rotation_dcm : NDArray | None, optional
+            Direction Cosine Matrix (DCM) representing rotation from camera to IMU. If provided,
+            `imu_camera_rotation_euler` must not also be provided.
+        imu_camera_rotation_euler : NDArray | None, optional
+            Euler angles (yaw, pitch, roll), in radians, representing rotation from camera to IMU.
+            If provided, `imu_camera_rotation_dcm` must not also be provided.
+        camera_altitude_offset : float, optional
+            Offset (in meters) between the real altitude and that measured by the IMU.
+            If the altitude above ground is larger than the IMU altitude, the offset is positive.
+        radius_of_influence : float | None, optional
+            Radius of neigbor pixels considered for nearest-neighbor resampling
+            (meters). If None, radius of influence is set to twice the ground sampling distance.
+        nodata_fill_value : _type_, optional
+            Which value to use for "no data" (outside swath), by default np.nan
+        ground_sampling_distance : float | None, optional
+            Ground sampling distance (in meters) for the orthorectified image. If None, GSD is
+            estimated from pixel coordinates.
+        estimate_yaw_from_positions: bool
+            Whether to estimate yaw from positions rather than using yaw measurements
+            from IMU. Useful if yaw measurements are inaccurate.
+        """
+
+        self.camera_model = CameraModel(
+            cross_track_fov=camera_fov,
+            n_pix=camera_cross_track_n_pixels,
+            R_to_imu_from_cam=imu_camera_rotation_dcm,
+            euler_to_imu_from_cam=imu_camera_rotation_euler,
+            altitude_offset=camera_altitude_offset,
+        )
+        self.resampler = Resampler(
+            radius_of_influence=radius_of_influence, nodata=nodata_fill_value
+        )
+        self.file_writer = GeoTiffWriter(nodata=nodata_fill_value)
+        self.gsd = ground_sampling_distance
+        self.estimate_yaw_from_positions = estimate_yaw_from_positions
+
+    def orthorectify_image(
+        self,
+        image: NDArray,
+        time: NDArray,
+        latitude: NDArray,
+        longitude: NDArray,
+        altitude: NDArray,
+        roll: NDArray,
+        pitch: NDArray,
+        yaw: NDArray | None = None,
+    ) -> tuple[NDArray, AreaDefinition, int]:
+        """Orthorectify hyperspectral image using IMU data
+
+        Parameters
+        ----------
+        image : NDArray
+            Hyperspectral image to be orthorectified, shape (n_lines, n_samples, n_bands).
+        time : NDArray
+            Time vector, shape (M,)
+        latitude : NDArray
+            Latitude in decimal degrees, shape (M,)
+        longitude : NDArray
+            Longitude in decimal degrees, shape (M,)
+        altitude : NDArray
+            Altitude above ground in meters, shape (M,)
+        roll : NDArray
+            Roll angles in radians, shape (M,)
+        pitch : NDArray
+            Pitch angles in radians, shape (M,)
+        yaw : NDArray | None, optional
+            Yaw angles in radians, shape (M,). If None, yaw is estimated from positions.
+
+        Returns
+        -------
+        tuple[NDArray, AreaDefinition, int]
+            ortho_image : NDArray
+                Orthorectified image, shape (n_northing, n_easting, n_bands).
+            area_def : AreaDefinition
+                Pyresample AreaDefinition for the orthorectified image.
+            utm_epsg : int
+                EPSG code for UTM zone of orthorectified image.
+
+        """
+        # Estimate yaw from positions if specified
+        easting, northing, utm_epsg = mpu.convert_long_lat_to_utm(longitude, latitude)
+
+        if (yaw is None) or self.estimate_yaw_from_positions:
+            yaw = heading_from_positions(time, northing, easting)
+
+        # Calculate pixel ground positions
+        pixel_utm_positions = self.camera_model.pixel_ground_positions(
+            camera_northing=northing,
+            camera_easting=easting,
+            camera_altitude=altitude,
+            camera_yaw=yaw,
+            camera_pitch=pitch,
+            camera_roll=roll,
+        )
+
+        # Create area and swath definitions
+        area_def = self.resampler._area_definition(pixel_utm_positions, utm_epsg)
+        swath_def = self.resampler._swath_definition(pixel_utm_positions, utm_epsg)
+
+        # Resample image to grid
+        gsd = self.gsd or resolution_from_pixel_coordinates(pixel_utm_positions)
+        ortho_image = self.resampler.resample(image, area_def, swath_def, gsd=gsd)
+
+        return ortho_image, area_def, utm_epsg
+
+    def orthorectify_image_file(
+        self,
+        image_path: Path | str,
+        imu_data_path: Path | str,
+        geotiff_path: Path | str,
+        rgb_only: bool = True,
+    ) -> None:
+        """Orthorectify hyperspectral image using IMU data and save result to file
+
+        Parameters
+        ----------
+        image_path : Path | str
+            Path to hyperspectral image header.
+        imu_data_path : Path | str
+            Path to IMU data file (JSON format).
+        geotiff_path : Path | str
+            Path to (output) GeoTIFF file.
+        rgb_only : bool, optional
+            Whether to only use RGB bands from hyperspectral image for orthorectification.
+        """
+        # Read image
+        image, wl, _ = mpu.read_envi(image_path)
+        if rgb_only:
+            image, wl = mpu.rgb_subset_from_hsi(image, wl)
+
+        # Read IMU data
+        imu_data = mpu.read_json(imu_data_path)
+
+        # Orthorectify image
+        ortho_image, area_def, utm_epsg = self.orthorectify_image(
+            image,
+            time=np.array(imu_data["time"]),
+            latitude=np.array(imu_data["latitude"]),
+            longitude=np.array(imu_data["longitude"]),
+            altitude=np.array(imu_data["altitude"]),
+            roll=np.array(imu_data["roll"]),
+            pitch=np.array(imu_data["pitch"]),
+            yaw=np.array(imu_data["yaw"]),
+        )
+        transform = from_bounds(
+            *area_def.area_extent, height=ortho_image.shape[0], width=ortho_image.shape[1]
+        )
+
+        # Save orthorectified image as GeoTIFF
+        self.file_writer.save_image(ortho_image, wl, transform, utm_epsg, geotiff_path)
+
+
+def resolution_from_pixel_coordinates(
     pixel_utm_coordinates: NDArray,
 ) -> float:
     """Estimate ground sampling distance (GSD) from pixel UTM coordinates.
@@ -386,3 +597,42 @@ def area_extent_from_pixel_coordinates(pixel_utm_positions: NDArray) -> tuple:
     utm_y_min = np.min(pixel_utm_positions[:, :, 0])
     utm_y_max = np.max(pixel_utm_positions[:, :, 0])
     return (utm_x_min, utm_y_min, utm_x_max, utm_y_max)
+
+
+def heading_from_positions(time: NDArray, northing: NDArray, easting: NDArray) -> NDArray:
+    """Calculate heading from smoothed positions
+
+    Parameters
+    ----------
+    time : NDArray
+        Time vector, shape (M,)
+    northing : NDArray
+        Northing in meters, shape (M,)
+    easting : NDArray
+        Easting in meters, shape (M,)
+
+    Returns
+    -------
+    NDArray
+        Heading (yaw) vector, in radians, shape (M,)
+
+    Raises
+    ------
+    ValueError
+        If input vectors are not all equal length
+    """
+
+    M = len(time)
+    if not all(len(x) == M for x in (northing, easting)):
+        raise ValueError("All input vectors must be of equal length.")
+
+    # Fit smoothing splines
+    sn = make_smoothing_spline(time, northing)
+    se = make_smoothing_spline(time, easting)
+
+    # First derivatives (velocity components)
+    dn_dt = sn.derivative()(time)
+    de_dt = se.derivative()(time)
+
+    # calculate heading
+    return np.arctan2(de_dt, dn_dt)  # arctan (easting / northing)
