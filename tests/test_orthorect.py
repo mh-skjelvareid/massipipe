f51857820ff57ce82fbe4e864b3f53070d20e2e3
Martin H. Skjelvareid <martin.hansen.skjelvareid@gmail.com>
2025-12-27 12:23:30 +0100

    Adding simple end-to-end test of orthorectification (no rotastions)

--- tests/test_orthorect.py
+++ tests/test_orthorect.py
@@ -2,7 +2,7 @@ import numpy as np
 import pytest
 from scipy.spatial.transform import Rotation
 
-from massipipe.orthorectification import CameraModel
+from massipipe.orthorectification import CameraModel, FlatTerrainOrthorectifier
 
 
 @pytest.fixture
@@ -30,19 +30,22 @@ def test_looking_angles(sample_angle, sample_camera_model):
     assert np.all(looking_angles == np.array([-sample_angle, 0, sample_angle]))
 
 
-def test_ray_rotation_dimensions(sample_angle, sample_camera_model, sample_imu_angles):
+def test_ray_rotation_dimensions(sample_camera_model, sample_imu_angles):
     M = len(sample_imu_angles[0])
     N = len(sample_camera_model.looking_angles)
     ray_rot_mat = sample_camera_model._ray_rotation_matrices(*sample_imu_angles)
     assert ray_rot_mat.shape == (M, N, 3, 3)
 
 
-def test_ray_rotation_roll(sample_angle, sample_camera_model, sample_imu_angles):
-    ray_rot_mat = sample_camera_model._ray_rotation_matrices(*sample_imu_angles)
-    a = sample_angle
+def test_ray_rotation_roll(sample_camera_model):
+    """Test rotation matrices for roll only (zero pitch and yaw)"""
+    roll = np.radians([-4, 0, 7, 15])
+    pitch = np.zeros_like(roll)
+    yaw = np.zeros_like(roll)
+    ray_rot_mat = sample_camera_model._ray_rotation_matrices(roll, pitch, yaw)
 
-    for phi, rot_mat_roll in zip([0, -a, a], ray_rot_mat[0:3]):
-        for alpha, rot_mat in zip([-a, 0, a], rot_mat_roll):
+    for phi, rot_mat_roll in zip(roll, ray_rot_mat):
+        for alpha, rot_mat in zip(sample_camera_model.looking_angles, rot_mat_roll):
             expected = np.array(
                 [
                     [1, 0, 0],
@@ -54,13 +57,15 @@ def test_ray_rotation_roll(sample_angle, sample_camera_model, sample_imu_angles)
             assert close_mask.all(), f"Matrices differ at indices:\n{np.argwhere(~close_mask)}"
 
 
-def test_ray_rotation_pitch(sample_angle, sample_camera_model, sample_imu_angles):
-    ray_rot_mat = sample_camera_model._ray_rotation_matrices(*sample_imu_angles)
-    a = sample_angle
+def test_ray_rotation_pitch(sample_camera_model):
+    pitch = np.radians([-4, 0, 7, 15])
+    roll = np.zeros_like(pitch)
+    yaw = np.zeros_like(roll)
+    ray_rot_mat = sample_camera_model._ray_rotation_matrices(roll, pitch, yaw)
 
     # Test rotation matrices for pitch / looking angles only (zero roll and yaw)
-    for theta, rot_mat_pitch in zip([-a, a], ray_rot_mat[3:5]):
-        for phi, rot_mat in zip([-a, 0, a], rot_mat_pitch):
+    for theta, rot_mat_pitch in zip(pitch, ray_rot_mat):
+        for phi, rot_mat in zip(sample_camera_model.looking_angles, rot_mat_pitch):
             expected = np.array(
                 [
                     [np.cos(theta), np.sin(phi) * np.sin(theta), np.cos(phi) * np.sin(theta)],
@@ -72,13 +77,15 @@ def test_ray_rotation_pitch(sample_angle, sample_camera_model, sample_imu_angles
             assert close_mask.all(), f"Matrices differ at indices:\n{np.argwhere(~close_mask)}"
 
 
-def test_ray_rotation_yaw(sample_angle, sample_camera_model, sample_imu_angles):
-    ray_rot_mat = sample_camera_model._ray_rotation_matrices(*sample_imu_angles)
-    a = sample_angle
+def test_ray_rotation_yaw(sample_camera_model):
+    yaw = np.radians([-4, 0, 7, 67, 193, 359])
+    roll = np.zeros_like(yaw)
+    pitch = np.zeros_like(yaw)
+    ray_rot_mat = sample_camera_model._ray_rotation_matrices(roll, pitch, yaw)
 
     # Test rotation matrices for yaw / looking angles only (zero roll and pitch)
-    for psi, rot_mat_yaw in zip([-a, a], ray_rot_mat[5:7]):
-        for phi, rot_mat in zip([-a, 0, a], rot_mat_yaw):
+    for psi, rot_mat_yaw in zip(yaw, ray_rot_mat):
+        for phi, rot_mat in zip(sample_camera_model.looking_angles, rot_mat_yaw):
             expected = np.array(
                 [
                     [np.cos(psi), -np.sin(psi) * np.cos(phi), np.sin(psi) * np.sin(phi)],
@@ -91,7 +98,7 @@ def test_ray_rotation_yaw(sample_angle, sample_camera_model, sample_imu_angles):
 
 
 def test_camera_to_ground_simple(sample_camera_model):
-    # Simple test case with no rotation and flat terrain
+    """Simple test case with no rotation and flat terrain"""
     imu_roll = np.array([0.0])
     imu_pitch = np.array([0.0])
     imu_yaw = np.array([0.0])
@@ -107,9 +114,10 @@ def test_camera_to_ground_simple(sample_camera_model):
         assert np.allclose(vector, expected_vector)
 
 
-def test_ground_offsets_no_yaw(sample_camera_model, sample_imu_angles):
-    # Test ground offsets without yaw
-    roll, pitch, _ = sample_imu_angles
+def test_ground_offsets_no_yaw(sample_camera_model):
+    """Test ground offsets without yaw"""
+    roll = np.radians([-21, -15, 0, 7, 15, 17])
+    pitch = np.radians([-9, -8, -2, 0, 10, 13])
     yaw = np.zeros_like(roll)
     H = 100.0
     altitude = np.ones_like(roll) * H
@@ -120,7 +128,11 @@ def test_ground_offsets_no_yaw(sample_camera_model, sample_imu_angles):
     for phi, theta, line_vectors in zip(roll, pitch, all_vectors):
         for alpha, pixel_vector in zip(sample_camera_model.looking_angles, line_vectors):
             expected_vector = H * np.array(
-                [np.tan(theta), (-np.tan(alpha + phi) / np.cos(theta)), 1]
+                [
+                    np.tan(theta),
+                    (-np.tan(alpha + phi) / np.cos(theta)),
+                    1,
+                ]
             )
             close_mask = np.isclose(pixel_vector, expected_vector)
             assert close_mask.all(), (
@@ -129,9 +141,11 @@ def test_ground_offsets_no_yaw(sample_camera_model, sample_imu_angles):
             )
 
 
-def test_ground_offsets_with_yaw(sample_camera_model, sample_imu_angles):
-    # Test ground offsets with yaw
-    roll, pitch, yaw = sample_imu_angles
+def test_ground_offsets_with_yaw(sample_camera_model):
+    """Test ground offsets with yaw"""
+    roll = np.radians([-21, -15, 0, 7, 15, 17])
+    pitch = np.radians([-9, -8, -2, 0, 10, 13])
+    yaw = np.zeros_like([-4, 0, 37, 88, 178, 352])
     H = 100.0
     altitude = np.ones_like(roll) * H
 
@@ -154,3 +168,49 @@ def test_ground_offsets_with_yaw(sample_camera_model, sample_imu_angles):
                 f"{pixel_vector=}, {expected_vector=}"
                 f"Vectors differ at indices:\n{np.argwhere(~close_mask)}, {phi=}, {theta=}, {psi=}"
             )
+
+
+def test_full_orthorect_no_rot():
+    """Test end-to-end pixel location calculation with no rotations
+
+    The camera is set to look straight down from 1000m altitude, so the ground
+    pixel locations should form a straight line along the north-south axis. Parameters
+    are adjusted so that the GSD is 100m along both axes. The image is placed along the
+    middle of UTM zone 1N, just north of the equator. The practical effect of orthorectification
+    in this case should be a flip along both axes (180 degree rotation).
+
+    """
+    image = np.arange(12).reshape((4, 3))
+    H = 1000.0  # 1000 meters altitude
+    alpha = 0.1
+    n_pix = 3
+    fov = 2 * alpha
+    gsd = H * np.tan(alpha)  # Approx 100 meters
+    altitude = np.array([H] * 4)
+    longitude = np.array([-177.0] * 4)  # Center of UTM zone 1N
+    latitude = np.array([1.0, 2.0, 3.0, 4.0]) * 0.0009047313695974662  # Around equator, ~100m steps
+    roll = np.zeros_like(altitude)
+    pitch = np.zeros_like(altitude)
+    yaw = np.zeros_like(altitude)
+    time = np.array([0.0, 1.0, 2.0, 3.0])
+
+    orthorect = FlatTerrainOrthorectifier(
+        camera_cross_track_fov=fov,
+        camera_cross_track_n_pixels=n_pix,
+    )
+
+    orthorect_image, area_def, utm_epsg = orthorect.orthorectify_image(
+        image,
+        time=time,
+        latitude=latitude,
+        longitude=longitude,
+        altitude=altitude,
+        roll=roll,
+        pitch=pitch,
+        yaw=yaw,
+    )
+
+    print(orthorect_image.shape)
+    assert orthorect_image.shape == (4, 3)
+    assert utm_epsg == 32601  # UTM zone 1N
+    assert np.allclose(orthorect_image, np.flipud(np.fliplr(image.astype(np.float32))))
